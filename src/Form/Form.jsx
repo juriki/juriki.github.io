79545bd2175e232a925e4ad04b0a1903a0d39258
Juri Tökvin <juri.tokvin@student.careeria.fi>
2023-11-13 22:45:13 +0200

    input validation

--- src/Form/Form.jsx
+++ src/Form/Form.jsx
@@ -1,8 +1,28 @@
 import './Form.css'
-import React from 'react';
+import React, { useState } from 'react';
 
 function Form() {
+
+    const [nameLastname, setNameLastname] = useState('')
+    const [phone, setPhone] = useState('')
+    const [email, setEmail] = useState('')
+
+    const [city, setCity] = useState('')
+    const [message, setMessage] = useState('')
+
+    function lahettin(){
+        if (email.length === '' || nameLastname === ''  || message === '' ){
+            alert("Joku kentä on tyhja TEST DEBUG  TODO Delete ")
+            console.log("Error")
+        }else{
+            alert("Hei " + nameLastname + " Sinun puhelin " + phone + email + " meilä otamme yhtytä sinun!" )
+        }
+     
+    }
+
+
     return (
+
 
         <>
               <div className='divv'>
@@ -16,28 +36,29 @@ function Form() {
                         <div className='test1'>
                         <label >
                         <p className='LabelP'>Nimi*</p>
-                            <input className='inputNamePhone' type="text" placeholder='Etunimi Sukunimi' />
+
+                            <input className='inputNamePhone' type="text" placeholder='Etunimi Sukunimi' value={nameLastname} onChange={(e)=> setNameLastname(e.target.value)} />
                         </label>
                         <label>
                         <p className='LabelP'>Puhelinnumero*</p>
-                            <input className='inputNamePhone' type="text" placeholder='050 1234 567' />
+                            <input className='inputNamePhone' type="text" placeholder='050 1234 567' value={phone} onChange={(e)=> setPhone(e.target.value)} />
                         </label>
                         <br />
                         <label>
                         <p className='LabelP'>Sähköpostiosoite*</p>
-                            <input type="text" placeholder='sähköposti@osoitte.fi' />
+                            <input type="text" placeholder='sähköposti@osoitte.fi' value={email} onChange={(e)=> setEmail(e.target.value)}  />
                         </label>
                         <br />
                         <label>
                         <p className='LabelP'>Paikakunta</p>
-                            <input type="text" />
+                            <input type="text"  value={city} onChange={(e)=> setCity(e.target.value)} />
                         </label>
                         <br />
                         <label>
                         <p className='LabelP'>Viesti*</p>
-                            <textarea className='texArea' ></textarea>
+                            <textarea className='texArea'  value={message} onChange={(e)=> setMessage(e.target.value)}></textarea>
                         </label>
-                        <button className='NappiSoitta1'>Lähettä</button>
+                        <button className='NappiSoitta1' onClick={lahettin}>Lähettä</button>
                         </div>
 
                     <br />
