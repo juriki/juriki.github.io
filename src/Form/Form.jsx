9b3cb1c2f9031ce81d8e85dce264bf6b3c6fead9
Juri Tökvin <juri.tokvin@student.careeria.fi>
2023-11-19 23:19:07 +0200

    form refactor

--- src/Form/Form.jsx
+++ src/Form/Form.jsx
@@ -24,54 +24,57 @@ function Form() {
 
 
         <>
-            <div className='divv'>
+            <div className='formMAindiv'>
+                <div className='mainText'>
                 <p className='Yhteys'>
                     Pyydä Tarjous:
                 </p>
-                <div className='UpForm'>
-                    <h6 >
-                        Jos tarvitset tarjouksen, ota rohkeasti yhteyttä
-                    </h6>
-                    <div className='test1'>
-                        <label >
-                            <p className='LabelP'>Nimi*</p>
+                </div>
 
-                            <input className='inputNamePhone' type="text" placeholder='Etunimi Sukunimi' value={nameLastname} onChange={(e) => setNameLastname(e.target.value)} />
-                        </label>
-                        <label>
-                            <p className='LabelP'>Puhelinnumero*</p>
-                            <input className='inputNamePhone' type="text" placeholder='050 1234 567' value={phone} onChange={(e) => setPhone(e.target.value)} />
-                        </label>
-                        <br />
-                        <label>
-                            <p className='LabelP'>Sähköpostiosoite*</p>
-                            <input type="text" placeholder='sähköposti@osoitte.fi' value={email} onChange={(e) => setEmail(e.target.value)} />
-                        </label>
-                        <br />
-                        <label>
-                            <p className='LabelP'>Paikakunta</p>
-                            <input type="text" value={city} onChange={(e) => setCity(e.target.value)} />
-                        </label>
-                        <br />
-                        <label>
-                            <p className='LabelP'>Viesti*</p>
-                            <textarea className='texArea' value={message} onChange={(e) => setMessage(e.target.value)}></textarea>
-                        </label>
-                        <br/>
-                        <button className='NappiSoitta1' onClick={lahettin}>Lähetä</button>
-                    </div>
+                <div className='upForm'>
+                    <h5 >
+                        Jos tarvitset tarjouksen, ota rohkeasti yhteyttä
+                    </h5>
+
+                    <label className='labelHalf' >
+                        <p className='LabelP'>Nimi*</p>
+                        <input className='inputNamePhone' type="text" placeholder='Etunimi Sukunimi' value={nameLastname} onChange={(e) => setNameLastname(e.target.value)} />
+                    </label>
+                    <label  className='labelHalf'>
+                        <p className='LabelP'>Puhelinnumero*</p>
+                        <input className='inputNamePhone' type="text" placeholder='050 1234 567' value={phone} onChange={(e) => setPhone(e.target.value)} />
+                    </label>
                     <br />
+                    <label className='labelFull'>
+                        <p className='LabelP'>Sähköpostiosoite*</p>
+                        <input className='Inputik' type="text" placeholder='sähköposti@osoitte.fi' value={email} onChange={(e) => setEmail(e.target.value)} />
+                    </label>
+                    <br />
+                    <label className='labelFull'>
+                        <p className='LabelP'>Paikakunta</p>
+                        <input className='Inputik'  type="text" value={city} onChange={(e) => setCity(e.target.value)} />
+                    </label>
+                    <br />
+                    <label className='labelFull'>
+                        <p className='LabelP'>Viesti*</p>
+                        <textarea className='Inputik' value={message} onChange={(e) => setMessage(e.target.value)}></textarea>
+                    </label>
+                    <br />
+                    <button className='NappiSoitta1' onClick={lahettin}>Lähetä</button>
                     <a href='https://www.facebook.com/ladislav.lahna/' target='_balnk'>
                         <img className='someLogos' src='facebook.png' alt="facebook" />
                     </a>
                     <a href='https://www.instagram.com/ladlahna/?hl=en' target='_balnk'>
-                        <img className='someLogos' src='instagramLogo.png' alt="instagram"/>
+                        <img className='someLogos' src='instagramLogo.png' alt="instagram" />
                     </a>
                     <a href='tel:+35840000000'>
                         <img className='someLogos' src='phoneLogo.png' alt="call" />
                     </a>
                 </div>
+                <br />
+
             </div>
+
 
 
         </>
