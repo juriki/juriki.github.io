d6433f957a8ba34786fa73c8b93e172da1cb6382
Juri Tökvin <juri.tokvin@student.careeria.fi>
2023-11-20 14:32:28 +0200

    coffe Break

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -20,6 +20,11 @@ function Navibar() {
                             <Nav.Link href='/contacts'>Yhteystiedot</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
+                    <Navbar.Text style={{color:"white", paddingRight: "10%", fontWeight: "200"}}>
+                        <h2>
+                        VL-Rakennus-ja-valvonta
+                            </h2>
+                    </Navbar.Text>
                     <Navbar.Brand href="/">
                         <img className='Logo' src='VladLogo.png' alt="nice" />
                     </Navbar.Brand>
