a529e05d479b65addc1c249c84347fe136a03766
Juri Tökvin <juri.tokvin@student.careeria.fi>
2023-11-20 13:41:02 +0200

    tables added to Hinnasto

--- src/AboutPage/Aboute.jsx
+++ src/AboutPage/Aboute.jsx
@@ -1,63 +1,45 @@
 //import UncontrolledExample from '../Components/Carousel';
+import PriceList from '../PriceList/PriceList';
 import './Aboute.css'
 import React from 'react';
 
 
 
 function Aboute() {
+    const lattia = ["Vinyyli / Laminaatti", "Parketti ", "Muovimatto"]
+    const lattiaHinta = ["20e / m2", "40e / m2 ", "25e / m2"]
+    const lattiaText = "(Hinta koostuu pelkästään työn osuudesta, ilman purkua)" 
+
+    const vedeneristys = ["Alle 5 m2 ", "5-10 m2",]
+    const vedeneristysHinta = ["800e", "900e",]
+    const vedeneristysText = "(Työ + Materiaalit) (Pinta-ala lattiasta katsottuna, valmiille pohjille)"
+
+    const naopinnote =["Alle 10 m2 ","10-20 m2 "]
+    const naopinnoteHinta =["600e","700e"]
+    const naopinnoteText = "(Työ + Materiaalit)(Pinta-ala lattiasta katsottuna)"
+
+
+    const markatilavalvonta = ["Vedeneristystarkastus","3 Käyntiä (alkukartoitus, vedeneristys ja lopputarkastus)","Keittiöremontin valvonta","Neuvonta 1h"]
+    const markatilavalvontaHinta = ["300e","600e","250e","180e"]
+    const markatilavalvontaText  = ""
+
+    const silikoninVaihto = ["Alle 5m2","5-10 m2 "]
+    const silikoninVaihtoHinta = ["300e","350e"]
+    const silikoninVaihtoText = "(Työ + Materiaalit) (Pinta-ala lattiasta katsottuna)"
+
+
+    const mikrosementti = ["Alle 5 m2 ","5-10 m2 "]
+    const mikrosementtiHinta = ["4500e","5500e"]
+    const mikrosementtiTekst = "(Työ + Materiaalit)(Pinta-ala lattiasta katsottuna)(Laattojen päälle) "
     return (
         <>
-            <div>
-
-                <div className='test'>
-                    <p>
-                        Hinnat sisältävät alv
-                        lattiat : (hinta koostuu pelkästään työn osuudesta, ilman purkua)
-                        <br />
-                        vinyyli / laminaatti 20e/m2
-                        <br />
-                        parketti 40e/m2
-                        <br />
-                        muovimatto 25e/m2
-                        <br />
-                        vedeneristystyöt: (työ + materiaalit)
-                        <br />
-                        pinta-ala lattiasta katsottuna, valmiille pohjille alle 5m2 800e
-                        <br />
-                        5-10m2 900e
-                        <br /> <br />
-                        Nanopinnoite: (työ + materiaalit)
-                        <br />
-                        pinta-ala lattiasta katsottuna alle 10 m2 600e
-                        <br />
-                        10-20 m2 700e
-                        <br /> <br />
-                        märkätilatöiden valvonta:
-                        <br />
-                        vedeneristystarkastus 300e
-                        <br /><br />
-                        3 käyntiä (alkukartoitus, vedeneristys, lopputarkastus) 600e keittiö remontin valvonta 250e
-                        <br />
-                        neuvonta (1h) 180e
-                        <br /><br />
-                        Silikonin vaihto: (työ + materiaali)
-                        <br />
-                        pinta-ala lattiasta katsottuna alle 5m2 300e
-                        <br />
-                        5-10m2 350e
-                        <br /><br />
-                        mikrosementti: (työ + materiaalit)
-                        <br />
-                        pinta-ala lattiasta katsottuna alle 5m2 4500e
-                        <br />
-                        5-10m2 5500e
-                        <br /><br />
-                        Pyydä tarjous<br />
-                        jos tarvitsen tarjouksen, ota rohkeasti yhteyttä<br />
-                        nimi sähköposti numero paikkakunta aihe viesti<br />
-                    </p>
-                </div>
-
+            <div style={{paddingTop: "200px"}}>
+                <PriceList palveluNimi="Lattia" otherText={lattiaText}  tyot={lattia} hinta={lattiaHinta}/>
+                <PriceList palveluNimi="VEDENERISTYS" otherText={vedeneristysText} tyot={vedeneristys} hinta={vedeneristysHinta}/>
+                <PriceList palveluNimi="NANOPINNOITE" otherText={naopinnoteText} tyot={naopinnote} hinta={naopinnoteHinta}/>
+                <PriceList palveluNimi="MÄRKÄTILATÖIDEN VALVONTA" otherText={markatilavalvontaText} tyot={markatilavalvonta} hinta={markatilavalvontaHinta}/>
+                <PriceList palveluNimi="SILIKONIN VAIHTO" otherText={silikoninVaihtoText} tyot={silikoninVaihto} hinta={silikoninVaihtoHinta}/>
+                <PriceList palveluNimi="MIKROSEMENTTI" otherText={mikrosementtiTekst} tyot={mikrosementti} hinta={mikrosementtiHinta}/>
             </div>
 
 
---
+++ src/PriceList/PriceList.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import './PriceList.css'
+
+function PriceList(props) {
+
+    var hinta = "Hinta"
+
+    return (
+        <>
+
+            <div>
+                <h3 style={{marginBottom: "-10px"}}>
+                    {props.palveluNimi}
+                </h3>
+                <p>{props.otherText}</p>
+                <table >
+                    <tr>
+                        <th>Työ</th>
+                        <th>Hina</th>
+                    </tr>
+                    {props.tyot.map((tyo, index) => {
+                        return (
+                            <tr>
+                                <td>{tyo}</td>
+                                <td>{props.hinta[index]}</td>
+                            </tr>
+                        )
+                    })}
+                </table>
+            </div>
+        </>
+    );
+}
+
+export default PriceList;
