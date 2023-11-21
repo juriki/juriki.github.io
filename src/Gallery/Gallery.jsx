fb14bfc996e06e0b4641d17c7879a78ef81d4542
Juri Tökvin <juri.tokvin@student.careeria.fi>
2023-11-21 16:39:47 +0200

    image commit

--- src/Gallery/Gallery.jsx
+++ src/Gallery/Gallery.jsx
@@ -3,7 +3,7 @@ import './Gallery.css'
 
 function Galleri() {
 
-    const kuvat = ["vladPict1.png", "vladPict10.png", "vladPict11.png", "vladPict4.png", "vladPict5.png", "vladPict6.png", "vladPict7.png", "vladPict8.png", "vladPict9.png", "vladPict2.png", "vladPict3.png",]
+    const kuvat = ["vladPict2.png", "vladPict10.png", "vladPict11.png", "vladPict4.png", "vladPict3.png", "vladPict1.png", "vladPict7.png", "vladPict8.png", "vladPict9.png", "vladPict5.png", "vladPict6.png",]
 
 
     return (
