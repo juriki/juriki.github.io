d3f97ff15ccbdd2bda77667de4768862afffa6e5
Juri Tökvin <juri.tokvin@student.careeria.fi>
2023-11-28 15:18:22 +0200

    big upDate

--- src/Gallery/Gallery.jsx
+++ src/Gallery/Gallery.jsx
@@ -3,18 +3,31 @@ import './Gallery.css'
 
 function Galleri(props) {
 
+    function Form(e, kuva1) {
+
+        alert(e.src)
+          console.log('You clicked submit.'+ e.name);
+          console.log('You clicked submit.' + kuva1);
+    }
+
     return (
         <>
+                   <h3 style={{textAlign: 'center'}}>{props.tyoNimi}</h3>
               <div className='galleryMainDiv'>
+   
                 {props.kuvat.map((kuva) => {
-                    return (
+                    return (        
                         <div className='galleryDiv1'>
-                           <a href={kuva} >
-                           <img className='gallery' src={kuva} alt="Cinque Terre" />
+                           <a href={kuva}>
+                           <img className='gallery' src={kuva} alt="Cinque Terre" onClick={(e) => Form(e.currentTarget, kuva)} name={kuva} />
+                        
                             </a>
+                  
                         </div>
+                        
                     )
                 })}
+                
             </div>
         </>
     );
--- src/KuvatPage/Kuvat.jsx
+++ src/KuvatPage/Kuvat.jsx
@@ -1,16 +1,19 @@
 //import UncontrolledExample from '../Components/Carousel';
 //import PriceList from '../PriceList/PriceList';
 //import './KuvaGalleria.css'
-import React from 'react';
 import Galleri from '../Gallery/Gallery';
 
 function Kuvat() {
-    
-    const kuvat = ["/newImaiges/kuvaVlad1.png", "/newImaiges/kuvaVlad2.png", "/newImaiges/kuvaVlad3.png", "/newImaiges/kuvaVlad4.png", "/newImaiges/kuvaVlad5.png", "/newImaiges/kuvaVlad6.png", "/newImaiges/kuvaVlad7.png", "/newImaiges/kuvaVlad8.png", "/newImaiges/kuvaVlad9.png", "/newImaiges/kuvaVlad10.png", "/newImaiges/kuvaVlad11.png", "/newImaiges/kuvaVlad12.png", "/newImaiges/kuvaVlad13.png", "/newImaiges/kuvaVlad14.png", "/newImaiges/kuvaVlad15.png", "/newImaiges/kuvaVlad16.png",
-        "/newImaiges/kuvaVlad17.png", "/newImaiges/kuvaVlad18.png", "/newImaiges/kuvaVlad19.png", "/newImaiges/kuvaVlad20.png", "/newImaiges/kuvaVlad21.png", "/newImaiges/kuvaVlad22.png"]
 
-        
-    const kuvat2 = ["vladPict2.png", "vladPict10.png", "vladPict11.png", "vladPict4.png", "vladPict3.png", "vladPict1.png", "vladPict7.png", "vladPict8.png", "vladPict9.png", "vladPict5.png", "vladPict6.png"]
+    const kylppari = [ "/newImaiges/kyllpySauna1.png","/newImaiges/kyllpySauna2.png","/newImaiges/kyllpySauna3.png","/newImaiges/kyllpySauna4.png",] 
+    const toiletSuhkku = ["/newImaiges/toiletSuhku1.png","/newImaiges/toiletSuhku2.png","/newImaiges/toiletSuhku3.png","/newImaiges/toiletSuhku4.png",]
+    const keittio = ["/newImaiges/ketio1.png","/newImaiges/ketio2.png","/newImaiges/ketio3.png","/newImaiges/ketio4.png","/newImaiges/ketio5.png",]
+    const bedRoom =["/newImaiges/bed2Room1.png","/newImaiges/bed2Room2.png","/newImaiges/bedRoom1.png","/newImaiges/bedRoom2.png",]
+    const stairs = ["/newImaiges/stairs1.png","/newImaiges/stairs2.png",]
+    const huoseOut = ["/newImaiges/HousOutDoor1.png","/newImaiges/HousOutDoor2.jpg","/newImaiges/HousOutDoor3.png",]
+    const terassi = ["/newImaiges/terassa1.png","/newImaiges/terassa2.png",]
+    const katto = ["/newImaiges/katto1.png","/newImaiges/katto2.png",]
+    const smalHouse = ["/newImaiges/smallHouse1.png","/newImaiges/smallHouse2.png"]
 
 
     return (
@@ -19,8 +22,15 @@ function Kuvat() {
             <h1 style={{paddingTop:"100px"}}>
   
             </h1>
-            <Galleri kuvat={kuvat}/>
-            <Galleri kuvat={kuvat2}/>
+           <Galleri kuvat={kylppari} tyoNimi={'Kylppyhuone'}/> 
+           <Galleri kuvat={toiletSuhkku} tyoNimi={'Suihku'}/> 
+           <Galleri kuvat={keittio}  tyoNimi={'Keitiö'}/> 
+           <Galleri kuvat={bedRoom}  tyoNimi={'Makuhuone'}/> 
+           <Galleri kuvat={stairs}  tyoNimi={'Portatiden saneraus'}/> 
+           <Galleri kuvat={huoseOut}  tyoNimi={'Julkisivut'}/> 
+           <Galleri kuvat={katto}  tyoNimi={'Kattoremontti'}/> 
+           <Galleri kuvat={terassi}  tyoNimi={'Terassi'} /> 
+           <Galleri kuvat={smalHouse}  tyoNimi={'Pien Rakennus'} /> 
         </>
 
     );
