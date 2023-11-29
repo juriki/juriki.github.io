66eacd268d90edb1dc90f9eaeaceccb440e7475f
Juri Tökvin <juri.tokvin@student.careeria.fi>
2023-11-29 21:46:35 +0200

    hurra

--- src/Footer/footer.jsx
+++ src/Footer/footer.jsx
@@ -4,8 +4,9 @@ function Footer() {
     return (
         <div className="Footer" >
              
-            Puhelin :<a href='tel:+35840000000'> 040-000 00 00 </a> <br/>
-            Sähköposti : <a href='mailto: lahna.vladislav@gmail.com'>lahna.vladislav@gmail.com</a>
+            Puhelin :<a href='tel:+358453400453'> 0453400453</a> <br/>
+            Sähköposti : <a href='mailto: vl.rakennus@gmail.com'>vl.rakennus@gmail.com</a><br/>
+            VL Valvonta ja Rakennus,  Y-tunnus : 3375062-5 
         </div>
     );
 }
