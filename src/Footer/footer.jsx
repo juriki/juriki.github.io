c800078f011080d81797b4fc4476482aee495ac4
Juri Tökvin <juri.tokvin@student.careeria.fi>
2023-12-12 13:28:59 +0200

    tekija tiedot

--- src/Footer/footer.jsx
+++ src/Footer/footer.jsx
@@ -7,6 +7,7 @@ function Footer() {
             Puhelin :<a href='tel:+358453400453'> 0453400453</a> <br/>
             Sähköposti : <a href='mailto: vl.rakennus@gmail.com'>vl.rakennus@gmail.com</a><br/>
             VL Valvonta ja Rakennus,  Y-tunnus : 3375062-5 
+            <p className='pNavBar'>Sivun tekija: <a href='mailto: juritokvin@gmail.com'>juritokvin@gmail.com</a> </p>
         </div>
     );
 }
